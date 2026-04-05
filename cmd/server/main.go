d9c2c0e6d1e619686df1b75aa0b5070effcc215e
pvdo <fire_ra@bk.ru>
2026-04-05 10:03:50 +0300

    cron works

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,19 +2,79 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"sync"
+
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-co-op/gocron/v2"
 )
 
+const httpPort = ":3000"
+
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("http://localhost:3000")
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		_, err := w.Write([]byte("welcome"))
+		if err != nil {
+			log.Println("Error writing response:", err)
+		}
 	})
-	http.ListenAndServe(":3000", r)
+
+	s, err := gocron.NewScheduler()
+	if err != nil {
+		panic(err)
+	}
+
+	jobs, err := initJobs(s)
+	if err != nil {
+		panic(err)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+
+		fmt.Println("You can click on link and open in browser")
+		fmt.Printf("http://localhost%s\n", httpPort)
+		err := http.ListenAndServe(httpPort, r)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		fmt.Printf("starting job: %d", jobs[0].ID())
+		s.Start()
+	}()
+
+	wg.Wait()
+}
+
+func initJobs(scheduler gocron.Scheduler) ([]gocron.Job, error) {
+	// add a job to the scheduler
+	j, err := scheduler.NewJob(
+		gocron.DurationJob(
+			10*time.Second,
+		),
+		gocron.NewTask(
+			func() {
+				fmt.Println("Cron job executed")
+			},
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return []gocron.Job{j}, nil
 }
