05af29aa8ce4ddf84da020b7d32e8ca6c019fb95
pvdo <fire_ra@bk.ru>
2026-04-06 09:27:36 +0300

    GetCoordinates

---
+++ internal/client/http/geocoding/client.go
@@ -0,0 +1,49 @@
+package geocoding
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type GeocodingResponse []struct {
+	Name      string  `json:"name"`
+	Country   string  `json:"country"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+type client struct {
+	httpClient http.Client
+}
+
+func NewClient(httpClient http.Client) *client {
+	return &client{
+		httpClient: httpClient,
+	}
+}
+
+func (c *client) GetCoordinates(city string) (res GeocodingResponse, err error) {
+	resp, err := c.httpClient.Get(
+		fmt.Sprintf("https://geocoding-api.open-meteo.com/v1/search?name=%s&count=1&language=ru&format=json",
+			city))
+	if err != nil {
+		return GeocodingResponse{}, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return GeocodingResponse{}, fmt.Errorf("failed to get coordinates: %s", resp.Status)
+	}
+
+	var geoResp struct {
+		Results GeocodingResponse `json:"results"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&geoResp)
+	if err != nil {
+		return GeocodingResponse{}, err
+	}
+
+	return geoResp.Results, nil
+
+}
--- notes.md
+++ notes.md
@@ -14,3 +14,8 @@ localhost:3000
 
 go get github.com/go-co-op/gocron/v2
 
+geocoding-api.open-meteo.com/v1/search?name=Moscow&count=1&language=ru&format=json
+
+api.open-meteo.com/v1/forecast?latitude=55.7558&longitude=37.6176&current=temperature_2m,apparent_temperature,relative_humidity_2m,weather_code,surface_pressure,wind_speed_10m,wind_direction_10m&timezone=auto
+
+https://api.open-meteo.com/v1/forecast?latitude=55.7558&longitude=37.6176&current=temperature_2m
